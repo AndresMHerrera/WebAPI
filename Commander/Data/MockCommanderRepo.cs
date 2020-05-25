89efeed93ab42a2a2f16c0e20a598e65d9f77185
andres herrera <aherrera004@outlook.com>
2020-05-25 14:18:12 -0700

    add repo

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command {Id=0, HowTo="list directory items.", Line="ls", Platform="OSX" },
+                new Command {Id=1, HowTo="make a directory.", Line="mkdir <directory name>", Platform="OSX" },
+                new Command {Id=2, HowTo="remove a directory.", Line="rm -rf <directory name>", Platform="OSX" },
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command {Id=0, HowTo="list directory items.", Line="ls", Platform="OSX" };
+        }
+    }
+}
